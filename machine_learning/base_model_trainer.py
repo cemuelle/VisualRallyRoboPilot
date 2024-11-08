9fc432df37443593831dc155b8a35bccab4a8028
Célien Muller <muller.celien@deverage.ch>
2024-11-08 15:41:37 +0100

    corr

--- machine_learning/base_model_trainer.py
+++ machine_learning/base_model_trainer.py
@@ -8,16 +8,7 @@ from preprocessing import preprocess
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 
-def trainer(model, training_dataloader, validation_dataloader, num_epochs, lr):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = AlexNetPerso(4, 0)
-    model.to(device)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
-    class_weights = torch.tensor([0.6, 2.0, 1.0, 1.0]).to(device)
-    criterion = nn.CrossEntropyLoss(weight=class_weights)
-
+def trainer(model, training_dataloader, validation_dataloader, num_epochs):
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     # writer = SummaryWriter('runs/pilot_trainer_{}'.format(timestamp))
     writer = None
@@ -28,7 +19,7 @@ def trainer(model, training_dataloader, validation_dataloader, num_epochs, lr):
     for epoch in range(num_epochs):
         print(f"Epoch {epoch + 1}")
         model.train()
-        avg_loss = train_one_epoch(epoch, writer)
+        avg_loss = train_one_epoch(epoch, training_dataloader, writer)
 
         running_loss = 0.0
 
@@ -37,7 +28,7 @@ def trainer(model, training_dataloader, validation_dataloader, num_epochs, lr):
         with torch.no_grad():
             for i, data in enumerate(validation_dataloader):
                 vinputs, vlabels = data
-                vinputs, vlabels = inputs.to(device), labels.to(device)
+                vinputs, vlabels = vinputs.to(device), vlabels.to(device)
 
                 voutputs = model(vinputs)
                 vloss = criterion(voutputs, vlabels)
@@ -60,7 +51,7 @@ def trainer(model, training_dataloader, validation_dataloader, num_epochs, lr):
             torch.save(model.state_dict(), model_path)
         
 
-def train_one_epoch(epoch_index, tb_writer=None):
+def train_one_epoch(epoch_index, training_dataloader, tb_writer=None):
     running_loss = 0.0
     last_loss = 0.0
 
@@ -94,17 +85,14 @@ def load_data():
     pass
 
 if __name__ == "__main__":
-    model = AlexNetPerso()
-    # Assuming your images are in a directory structure suitable for ImageFolder
-    dataset = ImageFolder(root='path_to_your_dataset', transform=preprocess)
-    
-    training_data, training_labels, validation_data, validation_labels = load_data()
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = AlexNetPerso(4, 0)
+    model.to(device)
 
+    optimizer = torch.optim.Adam(model.parameters())
 
-    training_dataset = data.TensorDataset(training_data, training_labels)
-    validation_dataset = data.TensorDataset(validation_data, validation_labels)
-    training_dataloader = data.DataLoader(training_dataset, batch_size=32, shuffle=True)
-    validation_dataloader = data.DataLoader(validation_dataset, batch_size=32, shuffle=False)
+    class_weights = torch.tensor([0.6, 2.0, 1.0, 1.0]).to(device)
+    criterion = nn.CrossEntropyLoss(weight=class_weights)
 
-    trainer(model, training_dataloader, validation_dataloader, 100, 0.001)
+    trainer(model, training_dataloader, validation_dataloader, 100)
