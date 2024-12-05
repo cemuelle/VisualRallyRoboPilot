f6bc304fe85dfaccb82f8a4235793a7c57c52675
Célien Muller <muller.celien@deverage.ch>
2024-12-05 10:25:20 +0100

    refactor code

--- machine_learning/base_model_trainer.py
+++ machine_learning/base_model_trainer.py
@@ -8,10 +8,8 @@ from torch.utils.data import DataLoader, random_split
 
 import os
 
-def trainer(model, training_dataloader, validation_dataloader, num_epochs, criterion=nn.BCEWithLogitsLoss()):
+def trainer(model, training_dataloader, validation_dataloader, num_epochs, criterion, optimizer, scheduler=None):
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    epoch_number = 0
-
     best_loss = float('inf')
 
     last_saved_model_path = None
@@ -23,83 +21,26 @@ def trainer(model, training_dataloader, validation_dataloader, num_epochs, crite
 
     for epoch in range(num_epochs):
         print(f"Epoch {epoch + 1}")
-        model.train()
-        avg_loss, avg_acc = train_one_epoch(training_dataloader, criterion)
-
-        running_loss = 0.0
-        correct_preds = 0
-        total_preds = 0
-
-        model.eval()
-
-        with torch.no_grad():
-            for i, data in enumerate(validation_dataloader):
-                inputs_image, inputs_speed, labels = data
-                vinputs_image, vinputs_speed, vlabels = inputs_image.to(device), inputs_speed.to(device), labels.to(device)
-                vinputs_speed = vinputs_speed.unsqueeze(1)
-
-                voutputs = model(vinputs_image, vinputs_speed)
         
-                vloss = criterion(voutputs, vlabels)
+        avg_loss, avg_acc = train_one_epoch(model, device, training_dataloader, criterion, optimizer)
+        avg_vloss, avg_vacc = validation_one_epoch(model, device, validation_dataloader, criterion)
 
-                running_loss += vloss.item()
-
-                prediction = torch.sigmoid(voutputs) > 0.5
-                correct_preds += (prediction == vlabels).sum().item()
-                total_preds += vlabels.numel()
-
-        avg_vloss = running_loss / len(validation_dataloader)
-        avg_vacc = correct_preds / total_preds
+        if scheduler is not None:
+            scheduler.step(avg_loss)
 
         print(f"Validation Loss: {avg_vloss:.4f}, Training Loss: {avg_loss:.4f}, Validation Accuracy: {avg_vacc:.4f}, Training Accuracy: {avg_acc:.4f}")
-        # print(f"predicted: {voutputs}, sigmoid : {torch.sigmoid(voutputs)}, actual: {vlabels}")
         training_loss.append(avg_loss)
         validation_loss.append(avg_vloss)
         training_accuracy.append(avg_acc)
         validation_accuracy.append(avg_vacc)
 
-        if avg_vloss < best_loss:
-            best_loss = avg_vloss
-            model_path = 'models/model_{}_{}.pth'.format(timestamp, epoch+1)
-            print(f"Saving model to {model_path}")
-            torch.save(model.state_dict(), model_path)
+        best_loss, last_saved_model_path = save_model(model, timestamp, epoch, avg_vloss, best_loss, last_saved_model_path)
 
-            # Remove the last saved model if it exists
-            if last_saved_model_path is not None:
-                if os.path.exists(last_saved_model_path):
-                    os.remove(last_saved_model_path)
-                    print(f"Removed previous model {last_saved_model_path}")
-
-            last_saved_model_path = model_path
-
-    # make a graph of the training and validation loss across epochs
-
-    import matplotlib.pyplot as plt
-
-    plt.figure(figsize=(12, 5))
-
-    # Plot Loss
-    plt.subplot(1, 2, 1)
-    plt.plot(training_loss, label="Training Loss")
-    plt.plot(validation_loss, label="Validation Loss")
-    plt.xlabel("Epoch")
-    plt.ylabel(f"Loss ({criterion.__class__.__name__})")
-    plt.legend()
-    plt.title("Training and Validation Loss")
-
-    # Plot Accuracy
-    plt.subplot(1, 2, 2)
-    plt.plot(training_accuracy, label="Training Accuracy")
-    plt.plot(validation_accuracy, label="Validation Accuracy")
-    plt.xlabel("Epoch")
-    plt.ylabel("Accuracy")
-    plt.legend()
-    plt.title("Training and Validation Accuracy")
-
-    plt.show()
+    plot_learning_curve(training_loss, validation_loss, training_accuracy, validation_accuracy)
         
 
-def train_one_epoch(training_dataloader, criterion):
+def train_one_epoch(model, device, training_dataloader, criterion, optimizer):
+    model.train()
     running_loss = 0.0
     correct_preds = 0
     total_preds = 0
@@ -111,7 +52,8 @@ def train_one_epoch(training_dataloader, criterion):
 
         optimizer.zero_grad()
 
-        outputs = model(vinputs_image , vinputs_speed)
+        outputs = model(vinputs_image, vinputs_speed)
+
         loss = criterion(outputs, vlabels)
         loss.backward()
 
@@ -127,6 +69,67 @@ def train_one_epoch(training_dataloader, criterion):
     avg_acc = correct_preds / total_preds
 
     return avg_loss, avg_acc
+
+def validation_one_epoch(model, device, test_loader, criterion):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total_preds = 0
+    with torch.no_grad():
+        for image, speed, target in test_loader:
+            image, speed, target = image.to(device), speed.to(device), target.to(device)
+            output = model(image, speed)
+
+            test_loss += criterion(output, target)
+
+            pred = torch.sigmoid(output) > 0.5
+            correct += (pred == target).sum().item()
+            total_preds += target.numel()
+    test_loss /= len(test_loader.dataset)
+
+    return test_loss, correct / total_preds
+
+def save_model(model, timestamp, epoch, avg_vloss, best_loss, last_saved_model_path):
+    if avg_vloss < best_loss:
+            best_loss = avg_vloss
+            model_path = 'models/model_{}_{}.pth'.format(timestamp, epoch+1)
+            print(f"Saving model to {model_path}")
+            torch.save(model.state_dict(), model_path)
+
+            # Remove the last saved model if it exists
+            if last_saved_model_path is not None:
+                if os.path.exists(last_saved_model_path):
+                    os.remove(last_saved_model_path)
+                    print(f"Removed previous model {last_saved_model_path}")
+
+            last_saved_model_path = model_path
+
+    return best_loss, last_saved_model_path
+
+def plot_learning_curve(training_loss, validation_loss, training_accuracy, validation_accuracy):
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=(12, 5))
+
+    # Plot Loss
+    plt.subplot(1, 2, 1)
+    plt.plot(training_loss, label="Training Loss")
+    plt.plot(validation_loss, label="Validation Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.title("Training and Validation Loss")
+
+    # Plot Accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(training_accuracy, label="Training Accuracy")
+    plt.plot(validation_accuracy, label="Validation Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.title("Training and Validation Accuracy")
+
+    plt.show()
 
 if __name__ == "__main__":
 
@@ -152,7 +155,8 @@ if __name__ == "__main__":
     training_dataloader = DataLoader(training_dataset, batch_size=8, shuffle=True)
     validation_dataloader = DataLoader(validation_dataset, batch_size=8, shuffle=False)
 
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=5, verbose=True)
 
     pos_weight = dataset.get_distribution().sum() / dataset.get_distribution()
     pos_weight[1] = pos_weight[1] * 0.3
@@ -162,4 +166,4 @@ if __name__ == "__main__":
     class_weights = torch.tensor(pos_weight).to(device)
     criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights)
 
-    trainer(model, training_dataloader, validation_dataloader, 100, criterion=criterion)
+    trainer(model=model, training_dataloader=training_dataloader, validation_dataloader=validation_dataloader, num_epochs=100, criterion=criterion, optimizer=optimizer, scheduler=scheduler)
