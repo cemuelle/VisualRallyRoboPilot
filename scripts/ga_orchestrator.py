a899bd53d088ad654a0e5c594ceb50a65cfe902f
le Seb du fond <113973829+SebastienBorloz@users.noreply.github.com>
2024-12-05 11:36:54 +0100

    more commenting and cleaning

--- scripts/ga_orchestrator.py
+++ scripts/ga_orchestrator.py
@@ -2,48 +2,59 @@ import os
 import json
 from ga_pipeline import genAl
 
-GENERATIONS = 20
-PITY = 20
+# parameters
+GENERATIONS = 20 # number of generations per iteration
+PITY = 20 # number of instructions to add at the end of the individual, it serves as a "with a few more instructions, it would have passed the gate" preventer
 
-def process_blocks(base_directory):
+
+def optimize_trajectories(base_directory):
     for context_dir in sorted(os.listdir(base_directory)):
+        # for each context directory in the base directory
         context_path = os.path.join(base_directory, context_dir)
         if os.path.isdir(context_path):
             print(f"Processing context: {context_dir}")
             
             for iteration_dir in sorted(os.listdir(context_path)):
+                # for each iteration directory in the current context directory
                 iteration_path = os.path.join(context_path, iteration_dir)
                 if os.path.isdir(iteration_path):
                     print(f"Processing iteration: {iteration_dir}")
                     
-                    # Collect all individuals (JSON files) in the current iteration block
+                    # Collect all individuals (JSON files) in the current iteration directory and aggregate all the individuals in a population list
                     population = []
                     context = {}
                     for file in sorted(os.listdir(iteration_path)):
                         if file.endswith('.json') and file.startswith('individual'):
                             file_path = os.path.join(iteration_path, file)
                             try:
-                                
                                 with open(file_path, 'r') as f:
                                     data = json.load(f)
-                                    print(len(context))
+                                    # checks if the context is the same for all individuals
                                     if len(context) == 0:
+                                        # if first, sets context
                                         context = data["initial_context"]
+                                        # formats the controls for the ga
                                         tupledControls = []
                                         for i in data["controls"]:
                                             tupledControls.append(tuple(i))
+                                        # adds the pity controls by duplicating the last controls {PITY} times
                                         for i in range(PITY):
                                             tupledControls.append(tupledControls[-1])
+                                        # adds the individual to the population
                                         population.append((iteration_path, tupledControls))
-
                                     elif context == data["initial_context"]:
+                                        # if not the first individual but the context matches
+                                        # formats the controls for the ga
                                         tupledControls = []
                                         for i in data["controls"]:
                                             tupledControls.append(tuple(i))
+                                        # adds the pity controls by duplicating the last controls {PITY} times 
                                         for i in range(PITY):
                                             tupledControls.append(tupledControls[-1])
+                                        # adds the individual to the population
                                         population.append((iteration_path, tupledControls))
                                     else:
+                                        # if the context doesn't match
                                         raise Exception(f"The context doesn't match the previous context {context} ({file_path})")
 
                             except json.JSONDecodeError as e:
@@ -51,13 +62,14 @@ def process_blocks(base_directory):
                             except Exception as e:
                                 print(f"    Error processing file {file_path}: {e}")
 
-                    print("finito le ", iteration_dir)
+                    # prints the resulting context and population
+                    print("Finished with interation ", iteration_path)
                     print(context)
                     print(population)
+                    # pass it through the GA pipeline
                     gaOut, scores, bests = genAl(GENERATIONS,population,context)
-                    print(scores)
-                    individual_result_path = os.path.join(iteration_path, f"improvement_{iteration_dir}.txt")
-                    generational_best_path = os.path.join(iteration_path, f"bests_{iteration_dir}.txt")
+
+                    # prints all the elits outputed by the GA in the right directory
                     for elit in range(len(gaOut)):
                         individual_file_path = os.path.join(iteration_path, f"ga_augmented_{iteration_dir}{elit}.json")
                         with open(individual_file_path, "w") as f:
@@ -65,12 +77,18 @@ def process_blocks(base_directory):
                                 "initial_context": context,
                                 "controls": gaOut[elit]
                             }, f, indent=4)   
+
+                    # stores the mean scores of the population at each generation
+                    individual_result_path = os.path.join(iteration_path, f"improvement_{iteration_dir}.txt")
                     with open(individual_result_path, "w") as f:
                         f.write(str(scores))
+
+                    # stores the best score of the population at each generation
+                    generational_best_path = os.path.join(iteration_path, f"bests_{iteration_dir}.txt")
                     with open(generational_best_path, "w") as f:
                         f.write(str(bests))
 
         
 
 base_directory = "out_red"  # Directory with the controls
-process_blocks(base_directory)
+optimize_trajectories(base_directory)
--- scripts/ga_pipeline.py
+++ scripts/ga_pipeline.py
@@ -24,6 +24,7 @@ car_position = [60, 0, 0]  # x, y, z
 car_speed = 30
 car_angle = -90
 
+# function to get all our usable pods ips
 def get_pods():
     v1 = client.CoreV1Api()
     pod_list = v1.list_namespaced_pod("isc3",label_selector='tier=headempty-zeb')
@@ -32,81 +33,41 @@ def get_pods():
         podIps.append(pod.status.pod_ip)
     return podIps
 
-
-def load_data(path):
-
-    files = glob.glob(path)
-    individual_controls = []
-
-    # Iterate through each file (representing one individual)
-    for i, file in enumerate(files):
-        with lzma.open(file, "rb") as f:
-            data = pickle.load(f)
-
-            # Collect all current_controls from the records in this file
-            controls = [record.current_controls for record in data if hasattr(record, 'current_controls')]
-
-            # Store in the list as a tuple (individual_id, controls)
-            individual_controls.append((f'individual_{i}', controls))
-
-    # Print the number of individuals and the stored controls
-    print(f"Total individuals: {len(individual_controls)}")
-    print("\nStored Controls:")
-
-    # Print all the current_controls for each individual
-    for individual, controls in individual_controls:
-        print(f"{individual}: {controls}")
-
-    return individual_controls
-
-def add_fitness(individual_controls, list_ips):
-    for idx, individual in enumerate(individual_controls):
-        if isinstance(individual, tuple):
-            if len(individual) == 3:  # Already has fitness
-                individual_id, controls, _ = individual
-            elif len(individual) == 2:  # Initial population
-                individual_id, controls = individual
-            else:
-                print(f"Unexpected tuple length at index {idx}: {individual}")
-                continue
-        else:
-            print(f"Unexpected entry at index {idx}: {individual}")
-            continue
-    # Evaluate the individual using the simulation
+# function to add fitness to a population, need the population and a list of ips on which to make simulation requests
+def add_fitness(individual_controls, list_ips, n_elits):
+    # Evaluate one individual using a pod for the simulation
     def send_single_request(list_controls, ip):
         global gate_p1, gate_p2, thickness, car_position, car_speed, car_angle
-        #print("hop la les controles: ")
-        #print(list_controls)
 
+        # the inputs can already have a score or not
         if isinstance(list_controls, tuple):
             if len(list_controls) == 3:  # Already has fitness
                 name, controlsRaw, _ = list_controls
             elif len(list_controls) == 2:  # Initial population
                 name, controlsRaw = list_controls
             else:
-                print(f"Unexpected tuple length at index {idx}: {individual}")
+                print(f"Unexpected tuple length at index {name}: {controlsRaw}")
         else:
-            print(f"Unexpected entry at index {idx}: {individual}")
+            print(f"Unexpected entry at index {name}: {controlsRaw}")
 
-        cond = True
-        while(cond):
-            reussied, status, time, col = send_simulation_request(
-                protocol="http",
-                server_ip=ip,
-                port=5000,
-                gate_p1=gate_p1,
-                gate_p2=gate_p2,
-                thickness=thickness,
-                car_position=car_position,
-                car_speed=car_speed,
-                car_angle=car_angle,
-                list_controls=controlsRaw,
-            )
-            #cond = not reussied
-            cond = False
-            print(reussied, status, time, col)
+        # request the score for the current individual
+        reussied, status, time, col = send_simulation_request(
+            protocol="http",
+            server_ip=ip,
+            port=5000,
+            gate_p1=gate_p1,
+            gate_p2=gate_p2,
+            thickness=thickness,
+            car_position=car_position,
+            car_speed=car_speed,
+            car_angle=car_angle,
+            list_controls=controlsRaw,
+        )
+        # prints the result
+        print(reussied, status, time, col)
 
-        #fitness = time if status and (col == 0) else 1000
+        # the fitness is the number of instructions it took to reach the gate + 50 * the number of colisions with walls.
+        # If it didn't reach the gate, the fitness is 1000
         fitness = time + 50 * col if status else 1000
         return (name,controlsRaw,fitness)
 
@@ -132,31 +93,39 @@ def add_fitness(individual_controls, list_ips):
         # Print results and return them
         print("resultats: ")
         for i in results:
-            print(i)
             name, controls, score = i
             print(name, nControls(i), score)
         return results
 
-
+    
     pop = len(individual_controls)
     while(True):
+        # loop to check if enough population went through the gate to make an elite
         newPop = run_simulation_in_parallel(pop,individual_controls)
-        cnt = 0
-        tries = 5
-        #print(newPop)
+        cnt = 0 # count of the number of individuals that passed the gates
+        tries = 5 # number of attempts before accepting a bad population
+
+        
         for i in newPop:
+            # for each individual in scored population
             name, controls, score = i
             if score != 1000:
+                # counts the individuals that passed the gate
                 cnt += 1
-            if cnt >= 2 or tries <= 0:
+
+            if cnt >= n_elits or tries <= 0:
+                # if we have enough elits or if we have already tried {tries} times, continue
                 return newPop
-            tries -= 1
+            
+        tries -= 1
         print("generation with no one passing the finish line, there must have been a problem. let's do it again.")
 
 
+# function to represent an individual by the sum of its controls
+# mainly a debug function to check if modifications occur each generation, simpler with this than with huge (0-1,0-1,0-1,0-1) lists
 def nControls(ind):
     a,w,s,d = 0,0,0,0
-    controls = individual_controls[0][1]
+    controls = ind[0][1]
 
     for i in controls:
         na,nw,ns,nd = i
@@ -194,7 +163,7 @@ def elitism(pop, elitism_count):
     THIS ALLOWS US TO REMOVE COMPLETELY FAILED INDIVIDUAL 
      WHICH FAILED THE SIMULATION PROCESS 
      SINCE WE CANNOT CONTROL THE TIMING OF THE SIMULATION
-         """
+"""
 def populate(elites, population_size, elitism_count):
     """
     Creates a population by replicating the elite individual.
@@ -222,62 +191,6 @@ def populate(elites, population_size, elitism_count):
     #print("Population:", populated)
     return populated
 
-
-"""  THE CROSS OVER """
-
-""" IT WAS REMOVED BECAUSE WE COULDNT NOT ENSURE THAT 
-    TWO INDIVIDUAL WERE AT THE SAME POINT IN THE TRAJECTORY
-    THUS BEING COUNTER PRODUCTIVE FOR THE EXPLOITATION AND EXPLORATION"""
-
-"""
-def crossover(parent1, parent2):
-    
-    #Perform crossover between two parents by randomly choosing control elements from each parent.
-
-    #Parameters:
-    #- parent1: List of control tuples for the first parent.
-    #- parent2: List of control tuples for the second parent.
-
-    #Returns:
-    #- child: New child created by selecting control elements from parent1 and parent2.
-    
-    child = []
-
-    for c1, c2 in zip(parent1, parent2):
-        # For each control element, randomly choose from parent1 or parent2
-        chosen_parent = random.choice([c1, c2])
-        child.append(chosen_parent)
-
-    return child
-
-def add_crossover_pop(pop, population_size=20, elite_count=4):
-
-    new_pop=[]
-
-    # Create new individuals using crossover until the population size is met
-    while len(new_pop) < (population_size - elite_count):
-        # Select two random parents from sorted population, ensuring they are not the same
-        parent1_data = random.choice(pop)
-        parent2_data = random.choice(pop)
-
-        # Ensure parent1 and parent2 are not the same individual
-        while parent1_data == parent2_data:
-            parent2_data = random.choice(pop)
-        # Unpack the controls from the parents
-        parent1_name, parent1_controls, parent1_fitness = parent1_data  # Unpack name and controls
-        parent2_name, parent2_controls, parent2_fitness = parent2_data
-        # Perform crossover between the parents
-        child_controls = crossover(parent1_controls, parent2_controls)
-
-        # Create the child with a random fitness score (this could be calculated differently)
-        #child_fitness_score = random.random()
-
-        # Store the child as a tuple (individual_name, controls, fitness_score)
-        #new_pop.append(('child_' + str(len(new_pop) + 1), child_controls, child_fitness_score))
-        new_pop.append(('child_' + str(len(new_pop) + 1), child_controls))
-
-    return new_pop
-"""
 
 
 """ THIS MUTATION WILL ALLOW EXPLORATION SINCE IT 
@@ -486,7 +399,7 @@ def genetic_algorithm(generation, mutation_rate, population_size, elitism_count,
     # Start the evolution process for the specified number of generations
     for gen in range(generation):
         print(f"\nGeneration {gen + 1}:")
-        individual_with_scores = add_fitness(individual_controls,game_ips)
+        individual_with_scores = add_fitness(individual_controls,game_ips, elitism_count)
         print(f"Fitness done {gen + 1}")
         # Step 0: store the best of the generation
         speeds_raw = [individual[2] for individual in individual_with_scores]
